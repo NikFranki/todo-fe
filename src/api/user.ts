2c68eae8ee2eb58de46c25f5906f0269239eb263
franki <8024ldbryant@gmail.com>
2023-12-30 20:41:18 +0800

    feat: add user api types

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,11 +1,11 @@
 import instance from '@utils/request';
 
-export const login = (params: any) => instance.post('/user/login', params);
+export const login = (params: { username: string; password: string; }) => instance.post('/user/login', params);
 
-export const logout = (params: any) => instance.post('/user/logout', params);
+export const logout = (params = {}) => instance.post('/user/logout', params);
 
-export const register = (params: any) => instance.post('/user/register', params);
+export const register = (params: FormData) => instance.post('/user/register', params);
 
-export const searchUser = (params: any) => instance.post('/user/searchUser', params);
+export const searchUser = (params = {}) => instance.post('/user/searchUser', params);
 
-export const updateUser = (params: any) => instance.post('/user/update', params);
+export const updateUser = (params: FormData) => instance.post('/user/update', params);
