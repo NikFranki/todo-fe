0ac0a1074e4815be53f635bc7defc7d0555df3a1
franki <8024ldbryant@gmail.com>
2023-12-30 22:40:35 +0800

    feat: add index.tsx types

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import App from './App';
 
 import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as any);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLHtmlElement);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
